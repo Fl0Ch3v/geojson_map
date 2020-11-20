96b7a1e0e089ef161d92b20ac4c3c05d8ba941d7
Fl0Ch3v <65029165+Fl0Ch3v@users.noreply.github.com>
2020-11-20 18:08:48 +0100

    avancement_com

--- mesFonctions.js
+++ mesFonctions.js
@@ -1,6 +1,6 @@
 function initialize() {
 		// création de la carte et paramétrage général : centre et niveau de zoom
-        var map = L.map('mapid').setView([50.172669, 1.802510], 12);
+        var map = L.map('mapid').setView([50.172669, 1.802510], 10);
  
 		// création d'une couche "osmLayer"
         var osmLayer = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -24,7 +24,7 @@ function initialize() {
 		onEachFeature: function( feature, layer )
 				{
 				// paramétrage de la popup de la couche "pnr"	
-				layer.bindPopup( "<b><u>Parc Naturel Régional</u></b><br><b><u>Baie de Somme - Picaride maritime</u></b><br>" + feature.properties.nb_com + " communes adhérentes<br>" + feature.properties.area_ha + "hectares" )
+				layer.bindPopup( "<b><u>Parc Naturel Régional</u></b><br><b><u>Baie de Somme - Picaride maritime</u></b><br>" + feature.properties.nb_com + " communes adhérentes<br>" + feature.properties.area_ha + " hectares" )
 				}
 		}).addTo(map);
 		});
@@ -40,7 +40,7 @@ function initialize() {
 		onEachFeature: function( feature, layer )
 				{
 				// paramétrage de la popup de la couche "pah"	
-				layer.bindPopup( "<b><u>Pays d'Art et d'Histoire</u></b><br><b><u>Ponthieu - Baie de Somme</u></b>")
+				layer.bindPopup( "<b>Pays d'Art et d'Histoire</b><br><b>Ponthieu - Baie de Somme</b>")
 				}
 		}).addTo(map);
 		});
@@ -56,13 +56,9 @@ function initialize() {
 		L.geoJson(dataPoint,{
 			pointToLayer: function(feature,latlng){
 				var marker = L.marker(latlng,{icon: iconeProjet});
-				marker.bindPopup('<b><u>Description du point</u></b><br>'
-							   + "<b>Nom : </b>" + feature.properties.id+ '<br>' 
-							   + "<b>Nombre d'écrans : </b>" + feature.properties.ecrans+ '<br>' 
-							   + "<b>Nombre de fauteuils : </b>" + feature.properties.fauteuils+ '<br>'
-							   + "<b>Arts et essais ? </b>" + feature.properties.art_et_ess+ '<br>'
-							   + "<b>Adresse : </b>" + feature.properties.adresse+ '<br>'
-							   + "<b>Arrondissement : </b>" + feature.properties.arrondisse
+				marker.bindPopup('<b><u>Description du projet</u></b><br>'
+							   + "<b>Commune : </b>" + feature.properties.nom+ '<br>' 
+							   + "<b>Type de patrimoine : </b>" + feature.properties.Typo
 							   );
 				return marker;
 				}
